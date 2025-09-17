73d5577928fb2814b405694e448ca1812cab6d93
Jessica Le <z5600135@ad.unsw.edu.au>
2025-09-17 17:05:29 +1000

    feat: basic implementation complete

--- wam.py
+++ wam.py
@@ -3,6 +3,7 @@
 # UNSW WAM --> sum of (grade * no. units) / total units
 
 import sys
+import math
 
 calc = {}
 
@@ -11,9 +12,14 @@ total_units = 0
 total_grade = 0 
 
 for line in sys.stdin:
-    total_units += int(line.split(){1})
+    total_units += int(line.split()[1])
+    total_grade += float(line.split()[0]) * float(line.split()[1])
 
 
-print(total_grade, "divide by", total_units)
+print(f" You current WAM is {total_grade} divide by {total_units}")
+
+print(format(total_grade / total_units, '.3f'))
+
+
 
 
