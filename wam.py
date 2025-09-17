6f71166afded9ae6a7d3dfa0b752e2d24f36804b
Jessica Le <z5600135@ad.unsw.edu.au>
2025-09-17 16:53:20 +1000

    feat: wam calc backend func, using terminal LOL

---
+++ wam.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# UNSW WAM --> sum of (grade * no. units) / total units
+
+import sys
+
+calc = {}
+
+total_units = 0
+
+total_grade = 0 
+
+for line in sys.stdin:
+    total_units += int(line.split(){1})
+
+
+print(total_grade, "divide by", total_units)
+
+
