695c8165c32b421aee735f2a8e37c5a9a6151b55
Jessica Le <z5600135@ad.unsw.edu.au>
2025-09-20 15:13:59 +1000

    feat: trolling

--- wam.py
+++ wam.py
@@ -7,21 +7,24 @@
 import sys
 import math
 
-calc = {}
+# calc = {}
 
 total_units = 0
-
+linecount = 0 # not needed, but im mean
 total_grade = 0 
 
 for line in sys.stdin:
     total_units += int(line.split()[1])
     total_grade += float(line.split()[0]) * float(line.split()[1])
-
+    linecount += 1
+if linecount == 0:
+    print("Nothing inputted. Don't waste my time.")
+    sys.exit(0)
+if linecount == 1:
+    print("You are stupid and your wam is the only grade you've received thus far")
+    sys.exit(0)
+# TODO: error checking + testing files
 
 print(f" You current WAM is {total_grade} divide by {total_units}")
 
 print(format(total_grade / total_units, '.3f'))
-
-
-
-
